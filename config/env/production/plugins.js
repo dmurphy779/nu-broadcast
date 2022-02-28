9fcb7adad3904500de14cf3b2aab1269861079cd
murphyd <murphyd@newmanu.edu>
2022-02-28 16:48:40 -0600

    Added AWS S3 Upload Credentials

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       provider: 'aws-s3',
       providerOptions: {
         accessKeyId: env('AKIA43LHR2PWVYLKOTEP'),
-        secretAccessKey: env('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'),
+        secretAccessKey: env('/v9wy4W5ehqBUvaUFVC1hF4MttrtUYymn+q42Ifn'),
         region: env('us-east-1'),
         params: {
           Bucket: env('strapi-nu-broadcast-media'),
