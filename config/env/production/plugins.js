4553c3164f5f803594cd9f68d78afa55ede032bb
murphyd <murphyd@newmanu.edu>
2022-03-01 14:38:13 -0600

    Corrected AWS Credentials 238

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,14 +1,14 @@
-module.exports = ({ env }) => ({
+module.exports = ({env}) => ({
   // ...
   upload: {
     config: {
       provider: 'aws-s3',
       providerOptions: {
-        accessKeyId: env('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: env('AWS_ACCESS_SECRET'),
-        region: env('AWS_REGION'),
+        accessKeyId: env('AWS_ACCESS_KEY_ID', 'AKIA43LHR2PWQDGPITUV'),
+        secretAccessKey: env('AWS_ACCESS_SECRET', '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'),
+        region: env('AWS_REGION', 'us-east-1'),
         params: {
-          Bucket: env('AWS_BUCKET_NAME'),
+          Bucket: env('AWS_BUCKET_NAME', 'strapi-nu-broadcast-media'),
         },
       },
     },
