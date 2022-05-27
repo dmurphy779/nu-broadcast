3232a33a50245f55c9d05a66ccbb842a99686968
murphyd <murphyd@newmanu.edu>
2022-05-27 12:59:29 -0500

    te

--- config/admin.js
+++ config/admin.js
@@ -1,4 +1,7 @@
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env('API_TOKEN_SALT', 'e?]!_q!Q!&ZC26sY4I!@Bb)/3odqYG=lf1[!>vOd;-7=#u+3,7z9|<zw{gW?&fU$'),
+  },
   auth: {
     secret: env('ADMIN_JWT_SECRET', '3c07e5dc63571c135a56c6967d85ccf4'),
   },
