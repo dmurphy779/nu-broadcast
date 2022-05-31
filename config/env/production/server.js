d9c7cbd70c563aaa35beb9e46d871d236b182fc2
murphyd <murphyd@newmanu.edu>
2022-05-31 11:26:48 -0500

    moved APP_KEYS value to .env file

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   url: env('APP_URL'),
   proxy: true,
   app: {
-    keys: env.array('APP_KEYS', ['16652e28d3c0c373f65cbfb0f582d8f5', '3c0a6ce4791c2cb45e79aa7a1b5f875d'])
+    keys: env.array('APP_KEYS')
   }
 });
