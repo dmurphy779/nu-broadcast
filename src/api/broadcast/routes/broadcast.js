5ef2fd138a45cba7f460f6d0f939184d7d32930b
murphyd <murphyd@newmanu.edu>
2023-01-31 15:23:22 -0600

    Updated to Strapi v 4.6.0

--- src/api/broadcast/routes/broadcast.js
+++ src/api/broadcast/routes/broadcast.js
@@ -6,4 +6,21 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::broadcast.broadcast');
+module.exports = createCoreRouter(
+  'api::broadcast.broadcast',
+  ({ strapi }) => ({
+    async find(ctx) {
+      const { user } = ctx.state;
+      const entities = await strapi.db
+        .query("api::broadcast.broadcast")
+        .findMany({
+          where: {
+            links: user,
+          },
+          populate: true,
+        });
+      return entities;
+    },
+  })
+);
+
